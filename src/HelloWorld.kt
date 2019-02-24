d0e0414107621b1f3fd8af53e7a80e3619adf3f5
saleelkhanshaik <saleelkhan03.shaik@gmail.com>
2019-02-24 13:50:49 +0530

    Create class followed by .Kt ,and there is now .class file after compiling the file ,fun main() is main method,there is now static concept in kotlin, var is used to create varibile and vaal is used make constant varibaale in kotlin

---
+++ src/HelloWorld.kt
@@ -0,0 +1,19 @@
+fun main(args: Array<String>){
+    /*
+    * this comment line 1
+    * this ccomment line 2
+    * we can write about function like t*/
+    var myNumber = 10
+    println("My Number is "+myNumber)
+    var myDecimalValue = 3.3
+    println(myDecimalValue)
+    var myString  : String
+    myString = "helllo saleel"
+    myString ="Hello jaanu how r you"
+    val myConstantsString  : String
+    myConstantsString ="Hello worl"
+
+    println(myString)
+    print(myConstantsString)
+
+}
