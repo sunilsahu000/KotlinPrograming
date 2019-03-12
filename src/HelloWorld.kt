af58be06444c5a0c14f206f11cf67ab926ea3c8b
saleelkhanshaik <saleelkhan03.shaik@gmail.com>
2019-03-12 06:40:41 +0530

    Working When Condition stateMent(Its like Switch in Java)

--- src/HelloWorld.kt
+++ src/HelloWorld.kt
@@ -1,41 +1,74 @@
-import Details.PersonDetails
-
 fun main(args: Array<String>) {
 
-    var r1 = 1..5 //it contains the 1,2,3,4,5 .. double dots meaning assending order
-    var r2 = 5 downTo 1  // It contains the 5,4,3,2,1 downTo meaning desending order
-    var r3 = 10 downTo 1 step 2 // it conraints the 10,8,6,4,2 like this it step
-    var r4 = 1.rangeTo(20)//its like .. function 1..20
-    var r5 = 10.downTo(1)
-
-    var a = 20
-    var b = 10
-    var maxVal: Int
-    if (a > b) {
-        maxVal = a
-    } else {
-        maxVal = b;
+    var number: Int = 21
+    when (number) {
+        1 -> println("the Nummber is 1")
+        2 -> println("the Number is 2")
+        3 -> println("the umber is 3")
+        20 -> println("The Number is 20")
+        else -> println("It is like Default in Jva " + number)
     }
-    println("MAX VALUE " + maxVal)
 
-    //same thing we can write below
-
-    var x = 30
-    var y =40
-    var CheckCon:Int = if(x>y){
-        a
-        x
-    }else{
-        b
-        y
+    var a = 7;
+    when (a) {
+        1 -> {
+            //writing the multiple codes of lines
+            println("The number is " + 1)
+            println("The number is1 " + 1)
+            println("The number is 2" + 1)
+        }
+        2 -> {
+            println("The number is " + 1)
+            println("The number is1 " + 1)
+            println("The number is 2" + 1)
+        }
+        3 -> {
+            println("The number is " + 1)
+            println("The number is1 " + 1)
+            println("The number is 2" + 1)
+        }
+        0, 7, 9 -> {
+            println("The number is " + a)
+            println("The number is1 " + a)
+            println("The number is 2" + a)
+        }
+        else -> {
+            //here we can make default value with multiple lines
+        }
     }
-    println("MAX Values "+CheckCon)
+
+    var a1 :Char = 's'
+    when(a1){
+        'a','e','i','o','u' -> println("VOWELS ")
+        else -> println("CONSTONETS ")
+
+    }
 
 
 
+    var rangeValues = 1..20
+    if(5 in 1..20){
+        println("Rangevalues containes the 5 value")
+    }
+
+
+    var z = 123;
+    when(z){
+        1,0,2,3,5,120 -> println("The value is 0,1,2,3,5,120")
+        !in 120..130 -> println("The value is 120 and 130 betwenn range")
+        else -> println("The value is ")
+    }
+
+    //WHEN as Expression
+
+    var x1 = 1;
+    var str:String =
+    when(x1){
+        0,2,3 -> "The values 0,2,3"
+        1 -> "The value is 1"
+        else -> "The is not value for X"
+    }
+    println(str)
 
 
 }
-
-
-
