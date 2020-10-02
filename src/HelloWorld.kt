8c63b551097ef638a368ee48c824f43cc611b940
Sunil sahu <72176852+sunilsahu000@users.noreply.github.com>
2020-10-02 10:15:10 +0530

    String International change and spelling mistakes change

--- src/HelloWorld.kt
+++ src/HelloWorld.kt
@@ -4,14 +4,14 @@ fun main(args: Array<String>){
     * this ccomment line 2
     * we can write about function like t*/
     var myNumber = 10
-    println("My Number is "+myNumber)
+    println("My Number is $myNumber")
     var myDecimalValue = 3.3
     println(myDecimalValue)
     var myString  : String
     myString = "helllo saleel"
     myString ="Hello jaanu how r you"
     val myConstantsString  : String
-    myConstantsString ="Hello worl"
+    myConstantsString ="Hello world"
 
     println(myString)
     print(myConstantsString)
